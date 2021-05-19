26e4ce11a8e41fb09df7a4a4b25feb35c568d80a
GabrielGB <gbtav83@gmail.com>
2021-05-19 11:38:57 -0300

    M2-01-Estrutura de dados

---
+++ script.js
@@ -0,0 +1,27 @@
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+
+
+let name = "Gabriel"
+let idade = 37
+let ano = 2021
+let nascimento = (ano - idade )
+console.log( "Meu ano de nascimento é" , nascimento)
+
+let souhumano = "true"
+console.log("Eu sou humano" , souhumano)
+
+
+let oi = 25
+let tchau = 25
+console.log(oi + tchau)
